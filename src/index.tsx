a5a88d265664992a085c7d6391a478eb3099ee53
naoki <n.globe.us@gmail.com>
2022-06-06 16:28:35 +0900

    some fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <div style={{backgroundColor: '#ff0000'}}>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/hello" element={<Hello />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
