dffb59759c652e50dcc4055c353a8870ebe242c7
naoki <n.globe.us@gmail.com>
2022-07-27 13:26:42 +0900

    fix legend layout with mobile

--- src/LegendControl.tsx
+++ src/LegendControl.tsx
@@ -17,14 +17,32 @@ class LegendControl {
     this.perCentage = perCentage;
   }
 
+  mobileClass(){
+
+    if (!this.container) {
+      return;
+    }
+
+    if (window.innerWidth < 670) {
+      this.container.classList.add('maplibregl-compact', 'mapboxgl-compact');
+    } else {
+      this.container.classList.remove('maplibregl-compact', 'mapboxgl-compact');
+    }
+
+  }
+
   onAdd(map: any){
 
     this.map = map;
 
     this.container = document.createElement('div');
-    this.container.className = '';
+    this.container.className = 'maplibregl-ctrl maplibregl-ctrl-attrib mapboxgl-ctrl mapboxgl-ctrl-attrib';
+    this.container.id = 'maplibregl-ctrl-legend'
+
+    this.mobileClass();
+
     const fieldset = document.createElement('fieldset');
-    fieldset.className = 'checkbox maplibregl-ctrl';
+    fieldset.className = 'checkbox maplibregl-ctrl-attrib-inner mapboxgl-ctrl-attrib-inner';
 
     let legendText = '';
 
@@ -54,6 +72,25 @@ class LegendControl {
     fieldset.appendChild(attribution);
 
     this.container.appendChild(fieldset);
+
+    const button = document.createElement('button');
+    button.className = 'maplibregl-ctrl-attrib-button mapboxgl-ctrl-attrib-button maplibregl-ctrl-legend-button';
+    button.title = 'Toggle Legend';
+    button.ariaLabel = 'Toggle Legend';
+
+    this.container.appendChild(button);
+
+    window.addEventListener('resize', () => this.mobileClass());
+
+    button.addEventListener('click', () => {
+
+      if (!this.container) {
+        return;
+      }
+
+      this.container.classList.toggle('maplibregl-compact-show');
+    })
+
 
     return this.container;
 
